85350272d8887b1e2d046c1c0f552b0948fcf755
Heath Babb <hbabb@hbc-data.com>
2024-08-08 19:08:56 -0400

    KKA-10: feat:completed project configurations

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,58 @@
-import { defineConfig } from 'vite'
+// karliKingAlexander/frontend/vite.config.ts
+
+/** @type {import('vite').UserConfig} */
+import path from 'node:path'
 import react from '@vitejs/plugin-react'
+import autoprefixer from 'autoprefixer'
+import { defineConfig } from 'vite'
+import EnvironmentPlugin from 'vite-plugin-environment'
+
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    EnvironmentPlugin(['all']),
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "./src/styles/variables"; @import "./src/styles/mixins";',
+      },
+    },
+    postcss: {
+      plugins: [
+        autoprefixer
+      ],
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'src/app/main.tsx'),
+      },
+    },
+  },
+  preview: {
+    port: 3000,
+    strictPort: true,
+  },
+  server: {
+    port: 3000,
+    strictPort: true,
+    host: '0.0.0.0',
+    hmr: {
+      host: 'localhost',
+      port: 8080,
+    },
+    watch: {
+      usePolling: true,
+    },
+  },
 })
